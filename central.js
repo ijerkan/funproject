8ba225bcdb7b0ebde826a46b11d9aa60e4df59f2
ijerkan <ivanjerkovic.117@gmail.com>
2019-08-08 15:52:05 +0200

    created a small server project

---
+++ central.js
@@ -0,0 +1,24 @@
+/*const Pet = require('./pet');
+const Games = require('./videogames');
+
+const pet1 = new Pet('dog', 'Hubert', 35);
+pet1.inspection();
+
+const pet2 = new Pet('parrot', 'Crookey', 2);
+pet2.inspection();
+
+const game1 = new Games('Call Of Duty', 'First Person Shooter');
+game1.review();
+
+
+
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener', data));
+
+logger.log('Hello World');
+logger.log('Hi');
+logger.log('Hello');*/
--- index.js
+++ index.js
@@ -1,11 +1,69 @@
-const Pet = require('./pet');
-const Games = require('./videogames');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const pet1 = new Pet('dog', 'Hubert', 35);
-pet1.inspection();
+const server = http.createServer((req, res) => {
+    /*if(req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if(err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        })  
+    }*/
 
-const pet2 = new Pet('parrot', 'Crookey', 2);
-pet2.inspection();
+    //Build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
 
-const game1 = new Games('Call Of Duty', 'First Person Shooter');
-game1.review();
+    // Extension of file
+    let extname = path.extname(filePath);
+
+    //Initial content type
+    let contentType = 'text/html';
+
+    //Check ext and set content type
+    switch(extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    // Read File
+    fs.readFile(filePath, (err, content) => {
+        if(err){
+            if(err.code == 'ENOENT') {
+                //Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                })
+            }
+            else {
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        }
+        else {
+            //Success
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+
+        }
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
